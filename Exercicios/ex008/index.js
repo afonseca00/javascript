936d2c68122f8f7a7d93531c13a612b1a5ec365e
afonseca00 <113251401+afonseca00@users.noreply.github.com>
2024-06-07 21:22:08 +0100

    Criei exercios e desafio ex006/7/8/9/10/11/12/13 e d007/8/9

---
+++ Exercicios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Voce passou o limite de velocidade Multa!!!')
+}
+console.log('Conduza sempre com o cinto')
---
+++ Exercicios/ex009/index.js
@@ -0,0 +1,7 @@
+var pais = 'Portugal'
+
+if (pais == 'Portugal') {
+    console.log("Portugues")
+} else {
+    console.log("Estrangeiro")
+}
