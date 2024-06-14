e6f626fb88853d157ac43d4b8e11db6171de9bd3
afonseca00 <113251401+afonseca00@users.noreply.github.com>
2024-06-14 18:32:22 +0100

    Criei ex014 e ex015

---
+++ Exercicios/ex014/index.js
@@ -0,0 +1,21 @@
+var c = 1
+
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/*
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+*/
+
+/*
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+*/
---
+++ Exercicios/ex015/index.js
@@ -0,0 +1,6 @@
+console.log(`Vai começar...`)
+
+for ( var c = 0; c <= 3; c++) {
+    console.log(`Passou ${c}`)
+}
+console.log(`FIM!!!!`)
